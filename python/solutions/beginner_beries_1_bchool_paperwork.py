5fa3d2db49339474f7584b6e514ee697b390b691
avra <stefan.avra@gmail.com>
2020-10-05 21:35:52 +0200

    added Beginner Series #1 School Paperwork

--- python/solutions/beginner_beries_1_bchool_paperwork.py
+++ python/solutions/beginner_beries_1_bchool_paperwork.py
@@ -1,4 +1,6 @@
 """
+Beginner Series #1 School Paperwork
+
 Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 
 Your task is to calculate how many blank pages do you need.
